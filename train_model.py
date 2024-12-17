1ebfd18b1325f82dcce9e1b6064348e1140566cf
faiz raza <faiz.raza.dec@gmail.com>
2024-12-18 00:42:00 +0500

    .

---
+++ train_model.py
@@ -0,0 +1,17 @@
+import pickle
+
+from sklearn.datasets import fetch_openml
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+x, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+clf = RandomForestClassifier(n_jobs=-1)
+clf.fit(x_train, y_train)
+
+print(clf.score(x_test, y_test))
+
+with open('model.pkl', 'wb') as model_file:
+    pickle.dump(clf, model_file)
